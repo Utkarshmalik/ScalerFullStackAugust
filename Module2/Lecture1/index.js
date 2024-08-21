dad25e79273707602eb1ed8217e62e8b544ad4f4
utmalik <utmalik@amazon.com>
2024-08-21 23:04:01 +0530

    JS 1

---
+++ Module2/Lecture1/index.js
@@ -0,0 +1,61 @@
+
+
+console.log("Hey Hello How are you");
+
+var a = 30;
+
+console.log(a);
+
+
+a = "hello";    //Re-assignment 
+
+
+var a= "bye";   //Re-Declartion
+
+
+console.log(a);
+
+
+// Problems of Var keyword
+// The var keyword allows the re-declaration of variables.
+
+
+
+// let r = 10;
+
+// r = 30;
+
+// console.log(r);
+
+// let r = 100;
+
+// let y="a";
+
+
+// console.log(r);
+
+
+
+// const x =5;
+
+// x=10;
+
+// console.log(x);
+
+// const x=10;
+
+// console.log(x);
+
+
+// Dynamically Types : 
+//  In dynamically typed languages, the type of a variable is determined at runtime. This means that you don't need to declare the type of a variable explicitly, and the same variable can hold values of different types at different times
+JS
+
+
+//Statically Typed : 
+// In statically typed languages, the type of a variable is determined at compile-time, before the code is run. This means that you must declare the type of each variable explicitly, and the compiler checks type correctness before the program executes.
+
+JAVA C++
+
+// int x=5;
+// x="hello";
