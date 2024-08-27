56a66d4bc2da278b1ad46e9fae74a482f8ef29e3
utmalik <utmalik@amazon.com>
2024-08-27 23:01:40 +0530

    JS Lec 3

---
+++ Module2/Lecture3/q1.js
@@ -0,0 +1,55 @@
+
+
+// console.log(a);
+//  var a=5;
+
+// sayHi();
+
+//  function sayHi(){
+//     console.log("Hey Hii , How are you");
+// }
+
+
+
+
+// a=5; 
+// console.log(a);
+// var a;
+
+
+
+
+// console.log(b);
+// var a=5;
+
+
+// sayHi();
+
+// function sayHi(){
+//     console.log("Hey Hii , How are you");
+// }
+
+
+// sayHi();
+
+
+// var sayHi = function(){
+//     console.log("Hey Hii , How are you");
+// }
+
+
+
+
+//Hoisting
+
+console.log(a);
+ let a=5;
+
+
+//  During the creation phase, variables declared with let, const, and class are "hoisted" to the top of their scope (like variables declared with var), but they are not initialized. This means they exist in memory but cannot be accessed until the code execution reaches their declaration.
+
+// The time between the beginning of the scope and the actual declaration in the code is the Temporal Dead Zone for that variable.
+
+// If you try to access or use a variable that's in the TDZ, JavaScript throws a ReferenceError.
+
+// The TDZ ends as soon as the variable is declared 
---
+++ Module2/Lecture3/script.js
@@ -0,0 +1,25 @@
+
+
+var a = 5;
+var b = 3;
+
+
+function add(num1, num2){
+    var ans= num1 + num2;
+    return ans;
+}
+
+var addition = add(a,b);
+
+console.log(addition);
+
+
+
+
+
+
+
+
+
+// var addition = add(a,b);
+
